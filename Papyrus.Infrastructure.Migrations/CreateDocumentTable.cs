bb70bc0adc2e142d2a90505141dbb02603958af8
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-06-19 11:35:27 +0100

    Migration for Document Table creation

---
+++ Papyrus.Infrastructure.Migrations/CreateDocumentTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Papyrus.Infrastructure.Migrations
+{
+    using FluentMigrator;
+
+    [TimestampedMigration(2015, 6, 19, 11, 04)]
+    public class CreateDocumentTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Documents")
+                .WithColumn("Id")
+                .AsString(50)
+                .PrimaryKey().NotNullable()
+                .WithColumn("Title")
+                .AsString()
+                .NotNullable()
+                .WithColumn("Description")
+                .AsString(300)
+                .WithColumn("Content")
+                .AsString(3000)
+                .WithColumn("Language")
+                .AsString(15);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Documents");
+        }
+    }
+}
