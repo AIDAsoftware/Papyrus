0141500738da0bd49205b301cdc0f5655ca841b2
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-06-25 14:47:03 +0100

    Refactor: extract consts

--- Papyrus.Business/SqlDocumentRepository.cs
+++ Papyrus.Business/SqlDocumentRepository.cs
@@ -7,48 +7,56 @@ namespace Papyrus.Tests
 
     public class SqlDocumentRepository : DocumentRepository
     {
+        private const string Server = @"server=.\SQLExpress;database=Papyrus;trusted_connection = true";
+
+        private const string InsertSqlQuery = @"INSERT Documents(Id, Title, Description, Content, Language) 
+                                                VALUES (@id, @title, @desc, @content, @lang);";
+
+        private const string SelectSqlQuery = @"SELECT *" + "FROM [Documents]" + "WHERE Id = @Id;";
+
+        private const string UpdateSqlQuery = @"UPDATE Documents " + 
+                                                "SET Title = @Title, " + 
+                                                    "Description = @Description, " + 
+                                                    "Content = @Content, " + 
+                                                    "Language = @Language " + 
+                                                "WHERE Id = @Id;";
+
+
         public void Save(Document document)
         {
-            using (var connection = new SqlConnection(@"server=.\SQLExpress;database=Papyrus;trusted_connection = true"))
+            using (var connection = new SqlConnection(Server))
             {
                 connection.Open();
-                connection.Execute(@"INSERT Documents(Id, Title, Description, Content, Language) 
-                                VALUES (@id, @title, @desc, @content, @lang);", 
-                                new { id = document.Id, title = document.Title, desc = document.Description,
-                                    content = document.Content, lang = document.Language,
-                                });
+                connection.Execute(InsertSqlQuery, new {
+                    id = document.Id,
+                    title = document.Title,
+                    desc = document.Description,
+                    content = document.Content,
+                    lang = document.Language,
+                });
             }
             
         }
 
         public Document GetDocument(string id)
         {
-            using (var connection = new SqlConnection(@"server=.\SQLExpress;database=Papyrus;trusted_connection = true"))
+            using (var connection = new SqlConnection(Server))
             {
-                return connection.Query<Document>(@"SELECT *" +
-                                           "FROM [Documents]" +
-                                           "WHERE Id = @Id;", new {Id = "AnyId"})
-                                           .First();
+                return connection.Query<Document>(SelectSqlQuery, new {Id = "AnyId"}).First();
             }
         }
 
         public void Update(Document document)
         {
-            using (var connection = new SqlConnection(@"server=.\SQLExpress;database=Papyrus;trusted_connection = true"))
+            using (var connection = new SqlConnection(Server))
             {
-                connection.Query<Document>(@"UPDATE Documents " +
-                                           "SET Title = @Title, " +
-                                                "Description = @Description, " +
-                                                "Content = @Content, " +
-                                                "Language = @Language " +
-                                           "WHERE Id = @Id;", 
-                                           new {
-                                               Id = document.Id,
-                                               Title = document.Title,
-                                               Description = document.Description,
-                                               Content = document.Content,
-                                               Language = document.Language
-                                           });
+                connection.Query<Document>(UpdateSqlQuery, new {
+                    Id = document.Id,
+                    Title = document.Title,
+                    Description = document.Description,
+                    Content = document.Content,
+                    Language = document.Language
+                });
             }
         }
 
