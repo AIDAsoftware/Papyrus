41b195ae1e4d1d7fe28e19dff23692cab59d6a79
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-07-10 15:46:08 +0100

    Add DocumentController and method to save a document
    The Add method create a row in the database but all fields are nulls (except Id, because it is generated in Business)

---
+++ Papyrus.WebServices/Controllers/DocumentsController.cs
@@ -0,0 +1,22 @@
+﻿namespace Papyrus.WebServices.Controllers
+{
+    using System.Web.Http;
+    using Business.Documents;
+
+    public class DocumentsController : ApiController
+    {
+        [HttpPost]
+        public string Add(Document document)
+        {
+            var documentService = new DocumentService(new SqlDocumentRepository());
+            documentService.Create(document);
+            return "{ Message: Document created }";
+        }
+
+        public string Get()
+        {
+            return "Hello World!";
+        }
+         
+    }
+}
