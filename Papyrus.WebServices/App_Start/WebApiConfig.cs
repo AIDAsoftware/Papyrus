7d42768bf317d4080a7a1e9264818b95cf6de806
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-07-13 15:18:42 +0100

    Put both services in same port

--- Papyrus.WebServices/App_Start/WebApiConfig.cs
+++ Papyrus.WebServices/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace Papyrus.WebServices
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
