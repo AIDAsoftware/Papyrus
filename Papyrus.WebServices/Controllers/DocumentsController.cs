3a6f8525fbbaf0f74893de7875fb9999f8cb6b9c
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-07-13 15:22:26 +0100

    Create DocumentDto class

--- Papyrus.WebServices/Controllers/DocumentsController.cs
+++ Papyrus.WebServices/Controllers/DocumentsController.cs
@@ -1,15 +1,23 @@
 ﻿namespace Papyrus.WebServices.Controllers
 {
     using System.Web.Http;
+    using System.Web.Http.Results;
+    using System.Web.Mvc;
     using Business.Documents;
 
     public class DocumentsController : ApiController
     {
-        [HttpPost]
-        public string Add(Document document)
+        [System.Web.Http.HttpPost]
+        public string Add([FromBody] DocumentDto document)
         {
             var documentService = new DocumentService(new SqlDocumentRepository());
-            documentService.Create(document);
+            documentService.Create(
+                new Document()
+                    .WithContent(document.Content)
+                    .WithDescription(document.Description)
+                    .WithTitle(document.Title)
+                    .ForLanguage(document.Language)
+                );
             return "{ Message: Document created }";
         }
 
@@ -18,5 +26,13 @@
             return "Hello World!";
         }
          
+    }
+
+    public class DocumentDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string Language { get; set; }
     }
 }
