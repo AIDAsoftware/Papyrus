f450fa5b3cc1549d3e96b7ea418de9d6cffe8a08
Modesto San Juan <msanjuan@aidacanarias.com>
2015-07-16 11:42:11 +0100

    refactor: created OwinRunner abstract class and merged with Startup.cs

---
+++ Papyrus.Tests/WebServices/OwinRunner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using NUnit.Framework;
+using Owin;
+using Papyrus.WebServices;
+
+namespace Papyrus.Tests.WebServices {
+    [TestFixture]
+    public abstract class OwinRunner {
+        private IDisposable webServer;
+        protected const string baseAddress = "http://localhost:8889/papyrusapi/";
+
+        [SetUp]
+        public void SetUp() {
+            webServer = WebApp.Start<Startup>(url: baseAddress);
+        }
+
+        [TearDown]
+        public void TearDown() {
+            webServer.Dispose();
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            // Takes all the referenced DLLs in the project
+            // Finds all the WebApi controllers and runs them all
+            WebApiConfig.Register(config);
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
--- Papyrus.Tests/WebServices/RestApiTest.cs
+++ Papyrus.Tests/WebServices/RestApiTest.cs
@@ -3,32 +3,21 @@
     using System;
     using System.Net.Http;
     using FluentAssertions;
-    using Microsoft.Owin.Hosting;
     using NUnit.Framework;
     using Papyrus.WebServices.Controllers;
 
     [TestFixture]
-    public class RestApiTest
-    {
+    public class RestApiRunner : OwinRunner {
         [Test]
-        public async void hello_world()
-        {
-//            DocumentDto doc = new DocumentDto();
-            string baseAddress = "http://localhost:8889/papyrusapi/";
+        public async void hello_world() {
 
-            // Start OWIN host 
-            using (WebApp.Start<Startup>(url: baseAddress))
-            {
-                // Create HttpCient and make a request to api/values 
-                HttpClient client = new HttpClient();
+            var client = new HttpClient();
+            var response = client.GetAsync(baseAddress + "documents").Result;
+            var document = await response.Content.ReadAsAsync<DocumentDto>();
 
-                var response = client.GetAsync(baseAddress + "documents").Result;
-                var document = await response.Content.ReadAsAsync<DocumentDto>();
-
-                document.Title.Should().Be("Any");
-                Console.WriteLine(response);
-                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
-            }
+            document.Title.Should().Be("Any");
+            Console.WriteLine(response);
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
         } 
     }
 }
