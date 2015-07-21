f4edffec42a6514f5b8be83c45b2b264cc9eba6d
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-07-21 12:24:43 +0100

    Add To do list

---
+++ Papyrus.Tests/WebServices/Controllers/DocumentControllerShould.cs
@@ -0,0 +1,19 @@
+﻿namespace Papyrus.Tests.WebServices.Controllers
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DocumentControllerShould
+    {
+
+        // TODO:
+        //  when looking for an existent Document, it should return a DocumentDto with this Document's information
+        //  when looking for a no existent Document, it should return an error message
+        //  when getting all Documents, it should return a list of dtos corresponding to all existant documents
+        //  when creating a document, it should return a message which confirms creation
+        //  when update a document, it should return a message which confirms the update
+        //  when try to update a no existant document, it should return a message which confirms the update
+        //  when remove a document, it should return a message which confirms elimination
+        //  when try to remove a no exsitant document, it should return a fail message 
+    }
+}
