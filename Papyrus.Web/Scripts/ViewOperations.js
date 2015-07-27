0bc5b7b3e58672a9efd97682026af7c23c07f8c5
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-07-27 15:05:44 +0100

    Fill documents list with all documents in library when page is ready

--- Papyrus.Web/Scripts/ViewOperations.js
+++ Papyrus.Web/Scripts/ViewOperations.js
@@ -1,4 +1,12 @@
-﻿function createButtonClick() {
+﻿$(document).ready(function () {
+    new DocumentService().allDocuments().done(function(documents) {
+        for (var i = 0; i < documents.length; i++) {
+            $("#documents-list").append('<a href="#" class="list-group-item">' + documents[i].Title + '</a>');
+        }
+    });
+});
+
+function createButtonClick() {
     var document = {
         Title: $("#inputTitle").val(),
         Description: $("#inputDescription").val(),
