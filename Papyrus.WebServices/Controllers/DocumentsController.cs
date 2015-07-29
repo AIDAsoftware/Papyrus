7f0949709255b711772e63087a9f5b19dcfb0e3c
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-07-29 13:31:15 +0100

    Refactor: extract method

--- Papyrus.WebServices/Controllers/DocumentsController.cs
+++ Papyrus.WebServices/Controllers/DocumentsController.cs
@@ -24,6 +24,39 @@
             return DocumentDtoFrom(document);
         }
 
+        [HttpPost]
+        public async Task<HttpResponseMessage> Add([FromBody] DocumentDto documentDto)
+        {
+            var document = DocumentFrom(documentDto);
+            await documentService.Create(document);
+            return new HttpResponseMessage(HttpStatusCode.Created);
+
+        }
+
+        private static Document DocumentFrom(DocumentDto documentDto)
+        {
+            return new Document()
+                .WithTitle(documentDto.Title)
+                .WithContent(documentDto.Content)
+                .WithDescription(documentDto.Description)
+                .ForLanguage(documentDto.Language);
+        }
+
+        [HttpPut]
+        public async Task<HttpResponseMessage> Update(string id, [FromBody] DocumentDto documentDto)
+        {
+            var document = DocumentFrom(documentDto);
+            await documentService.Update(document);
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+
+        public async Task<DocumentDto[]> Get()
+        {
+            var documents = await documentService.AllDocuments();
+
+            return documents.Select(document => DocumentDtoFrom(document)).ToArray();
+        }
+
         private static DocumentDto DocumentDtoFrom(Document document)
         {
             return new DocumentDto()
@@ -34,39 +67,6 @@
                 Content = document.Content,
                 Language = document.Language
             };
-        }
-
-        [HttpPost]
-        public async Task<HttpResponseMessage> Add([FromBody] DocumentDto documentDto)
-        {
-            var document = new Document()
-                .WithTitle(documentDto.Title)
-                .WithContent(documentDto.Content)
-                .WithDescription(documentDto.Description)
-                .ForLanguage(documentDto.Language);
-            await documentService.Create(document);
-            return new HttpResponseMessage(HttpStatusCode.Created);
-
-        }
-
-        [HttpPut]
-        public async Task<HttpResponseMessage> Update(string id, [FromBody] DocumentDto documentDto)
-        {
-            var document = new Document()
-                .WithId(id)
-                .WithTitle(documentDto.Title)
-                .WithContent(documentDto.Content)
-                .WithDescription(documentDto.Description)
-                .ForLanguage(documentDto.Language);
-            await documentService.Update(document);
-            return new HttpResponseMessage(HttpStatusCode.Accepted);
-        }
-
-        public async Task<DocumentDto[]> Get()
-        {
-            var documents = await documentService.AllDocuments();
-
-            return documents.Select(document => DocumentDtoFrom(document)).ToArray();
         }
     }
 }
