518e35f2fb2a197e0bc5fd38ae3f328481660bf6
Ronny Ancorini <Ronnygx@gmail.com>
2015-08-12 12:44:50 +0100

    Refactor: extract AllDocuments View

--- Papyrus.Web/Controllers/DocumentsController.cs
+++ Papyrus.Web/Controllers/DocumentsController.cs
@@ -13,5 +13,10 @@
         {
             return View();
         }
+
+        public ActionResult AllDocuments()
+        {
+            return View();
+        }
     }
 }
--- Papyrus.Web/Scripts/IndexViewOperations.js
+++ Papyrus.Web/Scripts/IndexViewOperations.js
@@ -25,6 +25,7 @@ function refreshDocumentsList() {
 }
 
 function paintDocumentRowFor(document) {
+    //TODO: change static url
     var url = "http://localhost:8888/papyrus/documents/details/" + document.Id;
     $("#documents-list").append(
         '<a href="' + url + '" class="list-group-item">'
