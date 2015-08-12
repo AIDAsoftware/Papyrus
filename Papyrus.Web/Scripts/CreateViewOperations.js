895761fe705ce66a9962eda2b6f804682f3818b5
Ronny Ancorini <Ronnygx@gmail.com>
2015-08-12 16:40:56 +0100

    Refactor: extract method to new file

---
+++ Papyrus.Web/Scripts/CreateViewOperations.js
@@ -0,0 +1,9 @@
+﻿function createButtonClick() {
+    var document = {
+        Title: $("#inputTitle").val(),
+        Description: $("#inputDescription").val(),
+        Content: $("#inputContent").val(),
+        Language: $("#inputLanguage").val()
+    };
+    new DocumentService().createDocument(document);
+}
--- Papyrus.Web/Scripts/IndexViewOperations.js
+++ Papyrus.Web/Scripts/IndexViewOperations.js
@@ -2,18 +2,6 @@
     refreshDocumentsList();
 });
 
-function createButtonClick() {
-    var document = {
-        Title: $("#inputTitle").val(),
-        Description: $("#inputDescription").val(),
-        Content: $("#inputContent").val(),
-        Language: $("#inputLanguage").val()
-    };
-    new DocumentService().createDocument(document).done(function() {
-        refreshDocumentsList();
-    });
-}
-
 function refreshDocumentsList() {
     $("#documents-list").html("");
     new DocumentService().allDocuments().done(function (documents) {
