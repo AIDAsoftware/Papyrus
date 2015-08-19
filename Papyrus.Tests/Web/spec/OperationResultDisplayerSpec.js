ddfb1bde51b1100408a59aad87a0794d0d034f2c
Ronny Ancorini <Ronnygx@gmail.com>
2015-08-19 11:46:19 +0100

    green: show green confirmation message when a document is created

---
+++ Papyrus.Tests/Web/spec/OperationResultDisplayerSpec.js
@@ -0,0 +1,29 @@
+﻿// The Jasmine Test Framework
+/// <reference path="~/../Papyrus.Web/Scripts/jquery-1.11.3.min.js"/>
+/// <reference path="~/../Papyrus.Web/Scripts/OperationResultDisplayer.js"/>
+/// <reference path="~/Web/lib/jasmine-2.3.4/jasmine.js" />
+
+describe("OperationResultDisplayer", function () {
+
+    beforeEach(function () {
+        $("body").append('<div id="message-notifier" style="display: none"><h3></h3></div>');
+    });
+
+    it("show green confirmation message when a document is created", function() {
+        var displayer = new papyrus.OperationResultDisplayer();
+        var message = {
+            title: "Document created",
+            type: "success"
+        }
+        
+        displayer.displayMessage(message);
+        var message = $("#message-notifier"); //TODO: think a name
+        expect(message.children("h3").text()).toEqual("Document created");
+        expect(message.css("background-color")).toEqual("rgb(166, 215, 133)");
+        expect(message.css("display")).toEqual("inline-block");
+    });
+
+    afterEach(function() {                    
+        $("#message-notifier").remove();
+    });
+});
---
+++ Papyrus.Web/Scripts/OperationResultDisplayer.js
@@ -0,0 +1,22 @@
+﻿var papyrus = papyrus || {};
+
+(function(ns) {
+
+    function OperationResultDisplayer() {
+
+        var displayMessage = function(message) {
+            var displayColor = "rgb(166, 215, 133)";
+            $("#message-notifier").css("background-color", displayColor);
+            $("#message-notifier").children("h3").text(message.title);
+            $("#message-notifier").css("display", "inline-block");
+        };
+
+        return {
+            displayMessage: displayMessage
+        }
+
+    };
+
+    ns.OperationResultDisplayer = OperationResultDisplayer;
+
+})(papyrus);
