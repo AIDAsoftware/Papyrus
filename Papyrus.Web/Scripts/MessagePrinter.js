89cb3b81d844ae8f7158bb4219e5987f8136867c
Ronny Ancorini <Ronnygx@gmail.com>
2015-08-21 10:57:24 +0100

    Refactor: extract method

--- Papyrus.Web/Scripts/MessagePrinter.js
+++ Papyrus.Web/Scripts/MessagePrinter.js
@@ -4,14 +4,17 @@
     function messagePrinter(idWithSpecialChar) {
         var idWithoutSpecialChar = idWithSpecialChar.substr(1);
         $("body").prepend('<div id="' + idWithoutSpecialChar + '" style="display: none"><h3></h3></div>');
+
+        function colorForType(messageType) {
+            return (messageType == "success") ? "rgb(166, 215, 133)" : "rgb(204, 0, 13)";
+        }
+
         var print = function (message) {
             var notifierWidget = $(idWithSpecialChar);
-            var color = (message.type == "success") ? "rgb(166, 215, 133)" : "rgb(204, 0, 13)";
-            notifierWidget.css("background-color", color);
+            notifierWidget.css("background-color", colorForType(message.type));
             notifierWidget.children("h3").text(message.title);
             notifierWidget.css("display", "inline-block");
         }
-
         return {
             print: print
         }
