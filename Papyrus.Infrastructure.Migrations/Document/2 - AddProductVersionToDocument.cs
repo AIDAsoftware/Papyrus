1143f39b5f77580ac39bb3226b91eca70ebf0fe4
Modesto San Juan <msanjuan@aidacanarias.com>
2015-08-27 19:26:53 +0100

    Added ProducuctVersionId column to Documents table

---
+++ Papyrus.Infrastructure.Migrations/Document/2 - AddProductVersionToDocument.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace Papyrus.Infrastructure.Migrations.Document
+{
+    [TimestampedMigration(2015, 8, 27, 19, 04)]
+    public class AddProductVerstionToDocument : Migration
+    {
+        public override void Up() {
+            Alter.Table("Documents").AddColumn("ProductVersionId").AsString(50).NotNullable().PrimaryKey("PK_Documents");
+            Alter.Table("Documents").AlterColumn("Language").AsString(15).NotNullable();
+
+            Delete.PrimaryKey("PK_Documents").FromTable("Documents");
+            Create.PrimaryKey("DocumentsPK").OnTable("Documents").WithSchema("dbo")
+                .Columns("Id", "ProductVersionId", "Language");
+        }
+
+        public override void Down() {
+            Delete.Column("ProductVersionId").FromTable("Documents").InSchema("dbo");
+
+            Delete.PrimaryKey("DocumentsPK").FromTable("Documents");
+            Alter.Table("Documents").AlterColumn("Id").AsString(50).PrimaryKey().NotNullable();
+            Alter.Table("Documents").AlterColumn("Language").AsString(15);
+        }
+    }
+}
