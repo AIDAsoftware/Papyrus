34225cf559fc027749f153e457b78fb8f2b65b10
Modesto San Juan <msanjuan@aidacanarias.com>
2015-09-21 21:37:22 +0100

    Product constructor without parameters is now private

--- Papyrus.Business/Products/Product.cs
+++ Papyrus.Business/Products/Product.cs
@@ -8,7 +8,7 @@ namespace Papyrus.Business.Products
         public string Name { get; private set; }
         public string Description { get; private set; }
 
-        public Product() {
+        private Product() {
             GenerateAutomaticId();
         }
 
