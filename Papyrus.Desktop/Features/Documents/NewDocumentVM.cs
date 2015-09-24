be4fb0849f5fe63e3d33c386e40c4b9267ecf69d
Modesto San Juan <msanjuan@aidacanarias.com>
2015-09-24 22:54:25 +0100

    Change to compile in C# 5

--- Papyrus.Desktop/Features/Documents/NewDocumentVM.cs
+++ Papyrus.Desktop/Features/Documents/NewDocumentVM.cs
@@ -110,7 +110,7 @@ namespace Papyrus.Desktop.Features.Documents
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
