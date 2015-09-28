791b2d1c0f11c28d6f1eca7d312ecad579810908
Ronny Ancorini <Ronnygx@gmail.com>
2015-09-28 12:41:37 +0100

    red: save_a_topic_when_it_is_created

---
+++ Papyrus.Business/TopicService.cs
@@ -0,0 +1,15 @@
+﻿namespace Papyrus.Business
+{
+    public class TopicService
+    {
+        public TopicService(TopicRepository topicRepo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Create(Topic topic)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Papyrus.Tests/Business/TopicServiceShould.cs
+++ Papyrus.Tests/Business/TopicServiceShould.cs
@@ -1,5 +1,6 @@
 ﻿using NSubstitute;
 using NUnit.Framework;
+using Papyrus.Business;
 
 namespace Papyrus.Tests.Business
 {
@@ -15,6 +16,22 @@ namespace Papyrus.Tests.Business
         //   topic without ranges cannot be saved 
         //   topic with id cannot be saved 
         //   topic without id cannot be updated
+
+
+        [Test]
+        public void save_a_topic_when_it_is_created()
+        {
+            var topicRepo = Substitute.For<TopicRepository>();
+            var service = new TopicService(topicRepo);
+            var topic = new Topic()
+                .ForProduct("AnyProductId");
+            var anyVersionRange = new VersionRange(fromVersion: "AnyVersionId", toVersion: "AnotherVersionId");
+            topic.AddVersionRange(anyVersionRange);
+
+            service.Create(topic);
+
+            topicRepo.Received().Save(topic);
+        }
 
     }
 }
