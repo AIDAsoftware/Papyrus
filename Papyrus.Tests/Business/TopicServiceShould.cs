f48995f607fb8ef8c2f9c52f687953a876f2e3f8
Ronny Ancorini <Ronnygx@gmail.com>
2015-09-28 12:39:17 +0100

    Add To do list for topic service
     Here we start a parallel change to redesign

---
+++ Papyrus.Tests/Business/TopicServiceShould.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace Papyrus.Tests.Business
+{
+    [TestFixture]
+    public class TopicServiceShould
+    {
+        // TODO:
+        //   save topic 
+        //   get topic by Id
+        //   update topic
+        //   Get all topics
+        //   topic which is not related to a product cannot be saved
+        //   topic without ranges cannot be saved 
+        //   topic with id cannot be saved 
+        //   topic without id cannot be updated
+
+    }
+}
