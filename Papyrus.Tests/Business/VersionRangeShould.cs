93e595989e45499ab2caf8918723294227bdbe92
Ronny Ancorini <Ronnygx@gmail.com>
2015-09-29 15:04:34 +0100

    To do list for Version Range

---
+++ Papyrus.Tests/Business/VersionRangeShould.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Papyrus.Tests.Business
+{
+    [TestFixture]
+    public class VersionRangeShould
+    {
+        // TODO:
+        //   get the document for a given language
+        //   know if it contains a given version
+        // Examples:
+        //   Given a Range with two Languages (es, en) when I request for "es" it should give me its spanish document.
+        //   Given a Range which contains versions from 2 to 4 it should return:
+        //      - false when I request by the 1
+        //      - true when I request by the 2
+        //      - true when I request by the 3
+        //      - true when I request by the 4
+        //      - false when I request by the 5
+    }
+}
