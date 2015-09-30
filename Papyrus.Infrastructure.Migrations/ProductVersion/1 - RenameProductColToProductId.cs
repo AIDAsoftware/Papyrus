66096158652da082230ea9c837da3b2850678ec7
Ronny Ancorini <Ronnygx@gmail.com>
2015-09-30 11:18:06 +0100

    Migration: Rename Product Column To ProductId

---
+++ Papyrus.Infrastructure.Migrations/ProductVersion/1 - RenameProductColToProductId.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace Papyrus.Infrastructure.Migrations.Version
+{
+    [TimestampedMigration(2015, 09, 30, 11, 11)]
+    public class RenameProductColToProductId : Migration
+    {
+        public override void Up()
+        {
+            Rename.Column("Product").OnTable("ProductVersion").InSchema("dbo").To("ProductId");
+        }
+
+        public override void Down()
+        {
+            Rename.Column("ProductId").OnTable("ProductVersion").InSchema("dbo").To("Product");
+        }
+    }
+}
