7ecd3c79d16140bd1bc7ef4fba029cae3a7cb368
Ronny Ancorini <Ronnygx@gmail.com>
2015-09-30 11:03:31 +0100

    Migration: Create Topic Table

---
+++ Papyrus.Infrastructure.Migrations/Topic/1 - CreateTopicTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace Papyrus.Infrastructure.Migrations.Topic
+{
+    [TimestampedMigration(2015, 09, 30, 10, 49)]
+    public class CreateTopicTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Topic").InSchema("dbo")
+                .WithColumn("TopicId").AsString(50).PrimaryKey()
+                .WithColumn("ProductId").AsString(50).NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Topic").InSchema("dbo");
+        }
+    }
+}
