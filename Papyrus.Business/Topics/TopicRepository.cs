fa22c8d8516fdd6dc95cdfc3580d06099fa13e82
Ronny Ancorini <Ronnygx@gmail.com>
2015-10-06 15:55:29 +0100

    Add method to the interface

--- Papyrus.Business/Topics/TopicRepository.cs
+++ Papyrus.Business/Topics/TopicRepository.cs
@@ -1,8 +1,12 @@
-﻿namespace Papyrus.Business.Topics
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Papyrus.Business.Topics
 {
     public interface TopicRepository
     {
         void Save(Topic topic);
         void Update(Topic topic);
+        Task<List<TopicToShow>> GetAllTopicsToShow();
     }
 }
