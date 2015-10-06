c4daed99b36ff2ea40e137269efbe9a559461328
Ronny Ancorini <Ronnygx@gmail.com>
2015-10-06 12:16:57 +0100

    green: get_a_list_with_all_topics_to_show

--- Papyrus.Tests/Infrastructure/Repositories/SqlTopicRepositoryShould.cs
+++ Papyrus.Tests/Infrastructure/Repositories/SqlTopicRepositoryShould.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Threading.Tasks;
+using FluentAssertions;
 using NUnit.Framework;
 using Papyrus.Business;
 using Papyrus.Business.Topics;
@@ -10,7 +11,7 @@ namespace Papyrus.Tests.Infrastructure.Repositories
     public class SqlTopicRepositoryShould : SqlTest
     {
         // TODO:
-        //   get all topics to show with properties "last version range document name", "description of such document", "product"
+        //   it should disctinct by topic Id showing the title and description for the last document added to a Topic
 
         [SetUp]
         public async void TruncateDataBase()
@@ -25,19 +26,13 @@ namespace Papyrus.Tests.Infrastructure.Repositories
         [Test]
         public async void get_a_list_with_all_topics_to_show()
         {
-            await dbConnection.Execute(@"INSERT INTO Topic (TopicId, ProductId) VALUES ('AnyTopicId', 'AnyProductId');
+            await InsertTopic("AnyTopicId", "AnyProductId");
+            await InsertProduct("AnyProductId", "Opportunity");
+            await InsertProductVersion("FirstVersionOpportunity", "1.0", "20150801", "AnyProductId");
+            await InsertVersionRange("AnyRangeId", "FirstVersionOpportunity", "FirstVersionOpportunity", "AnyTopicId");
+            await InsertDocument("PrimerMantenimientoOpportunity", "Primer Mantenimiento", "Explicación",
+                                "Puedes llamar a los clientes que necesitan...", "es-ES", "AnyRangeId");
 
-                                        INSERT INTO Product(ProductId, ProductName) VALUES('AnyProductId', 'Opportunity');
-            
-                                        INSERT INTO ProductVersion(VersionId, VersionName, Release, ProductId)
-                                            VALUES('FirstVersionOpportunity', '1.0', '20150801', 'AnyProductId');
-            
-                                        INSERT INTO VersionRange(VersionRangeId, FromVersionId, ToVersionId, TopicId)
-                                            VALUES('AnyRangeId', 'FirstVersionOpportunity', 'FirstVersionOpportunity', 'AnyTopicId');
-            
-                                        INSERT INTO Document(DocumentId, Title, Description, Content, Language, VersionRangeId)
-                                            VALUES('PrimerMantenimientoOpportunity', 'Primer Mantenimiento', 'Explicación',
-                                                'Puedes llamar a los clientes que necesitan...', 'es-ES', 'AnyRangeId');");
             var topicRepository = new SqlTopicRepository(dbConnection);
 
             var topicsToShow = await topicRepository.GetAllTopicsToShow();
@@ -47,6 +42,67 @@ namespace Papyrus.Tests.Infrastructure.Repositories
                                                 t.ProductName == "Opportunity" && 
                                                 t.LastDocumentTitle == "Primer Mantenimiento" &&
                                                 t.LastDocumentDescription == "Explicación");
+        }
+
+        private async Task InsertDocument(string documentId, string title, string description, string content, string language, string rangeId)
+        {
+            await dbConnection.Execute(@"INSERT INTO Document(DocumentId, Title, Description, Content, Language, VersionRangeId)
+                                            VALUES(@DocumentId, @Title, @Description, @Content, @Language, @RangeId);",
+                                            new
+                                            {
+                                                DocumentId = documentId,
+                                                Title = title,
+                                                Description = description,
+                                                Content = content,
+                                                Language = language,
+                                                RangeId = rangeId
+                                            });
+        }
+
+        private async Task InsertVersionRange(string rangeId, string fromVersionId, string toVersionId, string topicId)
+        {
+            await dbConnection.Execute(@"INSERT INTO VersionRange(VersionRangeId, FromVersionId, ToVersionId, TopicId)
+                                            VALUES(@VersionRangeId, @FromVersionId, @ToVersionId, @TopicId);",
+                                            new
+                                            {
+                                                VersionRangeId = rangeId,
+                                                FromVersionId = fromVersionId,
+                                                ToVersionId = toVersionId,
+                                                TopicId = topicId
+                                            });
+        }
+
+        private async Task InsertProductVersion(string versionId, string versionName, string release, string productId)
+        {
+            await dbConnection.Execute(@"INSERT INTO ProductVersion(VersionId, VersionName, Release, ProductId)
+                                            VALUES(@VersionId, @VersionName, @Release, @ProductId);", 
+                                            new
+                                            {
+                                                VersionId = versionId,
+                                                VersionName = versionName,
+                                                Release = release,
+                                                ProductId = productId
+                                            });
+        }
+
+        private async Task InsertProduct(string productId, string productName)
+        {
+            await dbConnection.Execute(@"INSERT INTO Product(ProductId, ProductName) VALUES(@ProductId, @ProductName);",
+                                        new
+                                        {
+                                            ProductId = productId, 
+                                            ProductName = productName
+                                        });
+        }
+
+        private async Task InsertTopic(string topicId, string productId)
+        {
+            await dbConnection.Execute(@"INSERT INTO Topic (TopicId, ProductId) VALUES (@TopicId, @ProductId);", 
+                                        new
+                                        {
+                                            TopicId = topicId,
+                                            ProductId = productId
+                                        });
         }
     }
 }
