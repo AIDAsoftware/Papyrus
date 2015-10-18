df1f8abe9fe96b0fbe06195fdf6bd93a7d65c9c8
Ronny Ancorini <Ronnygx@gmail.com>
2015-10-18 13:15:53 +0100

    Add to do list to sqlTopicRepository update

---
+++ Papyrus.Tests/Infrastructure/Repositories/TopicRepository/SqlTopicRepositoryShouldWhenUpdateATopic.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Papyrus.Tests.Infrastructure.Repositories.TopicRepository
+{
+    [TestFixture]
+    public class SqlTopicRepositoryShouldWhenUpdateATopic
+    {
+        // TODO:
+        //  - removes old related versionRanges and Documents for given topic
+        //  - save all new version ranges for given topic
+        //  - save all documents for each of given topic's version ranges        
+    }
+}
