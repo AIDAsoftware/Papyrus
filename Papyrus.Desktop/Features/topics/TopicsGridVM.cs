9facfe61251833338ce62844f1c94d2d39a66c72
Ronny Ancorini <Ronnygx@gmail.com>
2015-10-20 00:46:26 +0100

    Change grid refresh button to use vm's command instead code behind.

--- Papyrus.Desktop/Features/topics/TopicsGridVM.cs
+++ Papyrus.Desktop/Features/topics/TopicsGridVM.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Threading.Tasks;
 using Papyrus.Business.Topics;
+using Papyrus.Desktop.Util.Command;
 
 namespace Papyrus.Desktop.Features.Topics {
     public class TopicsGridVM
@@ -15,6 +16,13 @@ namespace Papyrus.Desktop.Features.Topics {
         public TopicsGridVM()
         {
             TopicsToList = new ObservableCollection<TopicSummary>();
+            RefreshTopics = RelayAsyncSimpleCommand.Create(LoadAllTopics, CanLoadAllTopics);
+        }
+
+        private bool canLoad;
+        private bool CanLoadAllTopics()
+        {
+            return canLoad;
         }
 
         public TopicsGridVM(TopicRepository topicRepository) : this()
@@ -29,9 +37,13 @@ namespace Papyrus.Desktop.Features.Topics {
 
         private async Task LoadAllTopics()
         {
+            canLoad = false;
             TopicsToList.Clear();
             (await topicRepository.GetAllTopicsSummaries()).ForEach(topic => TopicsToList.Add(topic));
+            canLoad = true;
         }
+
+        public IAsyncCommand RefreshTopics { get; set; }
 
         public async void RefreshDocuments()
         {
