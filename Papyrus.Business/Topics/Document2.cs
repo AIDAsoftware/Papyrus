d5420c296e057f2ef9a6622eac4960b5987ee575
Ronny Ancorini <Ronnygx@gmail.com>
2015-10-19 09:58:25 +0100

    In Progress. Adding Language property to the Document

--- Papyrus.Business/Topics/Document2.cs
+++ Papyrus.Business/Topics/Document2.cs
@@ -9,12 +9,19 @@ namespace Papyrus.Business.Topics
         public string Description { get; }
         public string Content { get; }
         public string DocumentId { get; private set; }
+        public string Language { get; }
 
         public Document2(string title, string description, string content)
         {
             Title = title;
             Description = description;
             Content = content;
+        }
+
+        public Document2(string title, string description, string content, string language)
+            : this(title, description, content)
+        {
+            Language = language;
         }
 
         public Document2 WithId(string id)
