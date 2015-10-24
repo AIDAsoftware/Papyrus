338e569659adcd9d848628c808a846dd5432a0b5
Ronny Ancorini <Ronnygx@gmail.com>
2015-10-24 13:33:48 +0100

    Design mode ViewModel for VersionRangesUserControl

--- Papyrus.Desktop/Features/topics/VersionRangesVM.cs
+++ Papyrus.Desktop/Features/topics/VersionRangesVM.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using Papyrus.Business.Products;
 using Papyrus.Business.Topics;
 
 namespace Papyrus.Desktop.Features.Topics
 {
     public class VersionRangesVM
     {
-        public ObservableCollection<EditableVersionRange> VersionRanges { get; }
+        public ObservableCollection<EditableVersionRange> VersionRanges { get; protected set; }
 
         public VersionRangesVM()
         {
@@ -20,6 +22,34 @@ namespace Papyrus.Desktop.Features.Topics
         public void Initialize()
         {
             
+        }
+    }
+
+    public class DesignModeVersionRangesVM : VersionRangesVM
+    {
+        public DesignModeVersionRangesVM()
+        {
+            var visibleEditableVersionRange = new EditableVersionRange
+            {
+                FromVersion = new ProductVersion("AnyId", "1.0", DateTime.Today),
+                ToVersion = new ProductVersion("AnyId", "2.0", DateTime.Today),
+            };
+            visibleEditableVersionRange.Documents.Add(new EditableDocument
+            {
+                Title = "Título",
+                Description = "Descripción",
+                Content = "Contenido",
+                Language = "es-ES"
+            });
+            VersionRanges = new ObservableCollection<EditableVersionRange>
+            {
+                visibleEditableVersionRange,
+                new EditableVersionRange
+                {
+                    FromVersion = new ProductVersion("AnyId", "3.0", DateTime.Today),
+                    ToVersion = new ProductVersion("AnyId", "4.0", DateTime.Today)
+                }
+            };
         }
     }
 }
