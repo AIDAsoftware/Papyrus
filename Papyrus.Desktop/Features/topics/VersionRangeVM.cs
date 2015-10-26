5d697bbf4b1b4fccc4a2c38d1c31b61301d66a94
Ronny Ancorini <devnull@localhost>
2015-10-26 17:31:20 +0000

    Selecting spanish document by default when VersionRangeUserControl is open

--- Papyrus.Desktop/Features/topics/VersionRangeVM.cs
+++ Papyrus.Desktop/Features/topics/VersionRangeVM.cs
@@ -1,13 +1,45 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Papyrus.Business.Products;
 using Papyrus.Business.Topics;
+using Papyrus.Desktop.Annotations;
 
 namespace Papyrus.Desktop.Features.Topics
 {
-    public class VersionRangeVM
+    public class VersionRangeVM : INotifyPropertyChanged
     {
-        public EditableVersionRange VersionRange { get; set; }
         public ObservableCollection<ProductVersion> ProductVersions { get; private set; }
+
+        private EditableDocument selectedDocument;
+        public EditableDocument SelectedDocument
+        {
+            get
+            {
+                return selectedDocument;
+            }
+            set
+            {
+                selectedDocument = value;
+                OnPropertyChanged("SelectedDocument");
+            }
+        }
+
+        private EditableVersionRange versionRange;
+        public EditableVersionRange VersionRange
+        {
+            get
+            {
+                return versionRange;
+            }
+            set
+            {
+                versionRange = value;
+                OnPropertyChanged("VersionRange");
+                SelectDefaultDocument();
+            }
+        }
 
         public VersionRangeVM()
         {
@@ -18,6 +50,20 @@ namespace Papyrus.Desktop.Features.Topics
         {
             ProductVersions = new ObservableCollection<ProductVersion>();
             VersionRange = editableVersionRange;
+        }
+
+        private void SelectDefaultDocument()
+        {
+            SelectedDocument = VersionRange.Documents.First(d => d.Language == "es-ES");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
