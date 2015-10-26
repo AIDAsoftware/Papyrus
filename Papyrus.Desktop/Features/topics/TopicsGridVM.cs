a59adc7aaa9790a6589ab7af21fe4ffb19040db3
Ronny Ancorini <devnull@localhost>
2015-10-26 15:20:19 +0000

    Select first loaded product by default instead of hard code "OpportunityId"

--- Papyrus.Desktop/Features/topics/TopicsGridVM.cs
+++ Papyrus.Desktop/Features/topics/TopicsGridVM.cs
@@ -1,21 +1,34 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Papyrus.Business.Products;
 using Papyrus.Business.Topics;
+using Papyrus.Desktop.Annotations;
 using Papyrus.Desktop.Util.Command;
 
 namespace Papyrus.Desktop.Features.Topics {
-    public class TopicsGridVM
+    public class TopicsGridVM : INotifyPropertyChanged
     {
         private readonly TopicRepository topicRepository;
         private readonly ProductRepository productRepository;
         public ObservableCollection<TopicSummary> TopicsToList { get; protected set; }
         public ObservableCollection<DisplayableProduct> Products { get; private set; }
         public TopicSummary SelectedTopic { get; set; }
-        public DisplayableProduct SelectedProduct { get; set; }
+
+        private DisplayableProduct selectedProduct;
+        public DisplayableProduct SelectedProduct
+        {
+            get { return selectedProduct; }
+            set
+            {
+                selectedProduct = value;
+                OnPropertyChanged("SelectedProduct");
+            }
+        }
 
         public IAsyncCommand RefreshTopics { get; set; }
 
@@ -23,7 +36,6 @@ namespace Papyrus.Desktop.Features.Topics {
         {
             TopicsToList = new ObservableCollection<TopicSummary>();
             Products = new ObservableCollection<DisplayableProduct>();
-            SelectedProduct = new DisplayableProduct { ProductName = "Opportunity", ProductId = "OpportunityId" };
             RefreshTopics = RelayAsyncSimpleCommand.Create(LoadAllTopics, CanLoadAllTopics);
         }
 
@@ -36,6 +48,7 @@ namespace Papyrus.Desktop.Features.Topics {
         public async Task Initialize()
         {
             await LoadAllProducts();
+            SelectedProduct = Products[0];
             await LoadAllTopics();
         }
 
@@ -102,6 +115,14 @@ namespace Papyrus.Desktop.Features.Topics {
             editableVersionRange.Documents.Add(new EditableDocument { Language = "en-GB" });
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
     public class DesignModeTopicsGridVM : TopicsGridVM
