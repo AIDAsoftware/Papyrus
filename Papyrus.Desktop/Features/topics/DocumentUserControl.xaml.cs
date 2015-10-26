b9a36c81532ad736241b6b26b4e389ccfd3be170
Ronny Ancorini <devnull@localhost>
2015-10-26 17:01:19 +0000

    Add DocumentUserControl

---
+++ Papyrus.Desktop/Features/topics/DocumentUserControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Papyrus.Desktop.Features.Topics
+{
+    public partial class DocumentUserControl : UserControl
+    {
+        public DocumentUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
