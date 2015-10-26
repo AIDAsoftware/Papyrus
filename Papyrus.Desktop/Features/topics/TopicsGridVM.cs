937dd2a7ecd93aba6d4b9ee2c8f7d952f4100c86
Ronny Ancorini <devnull@localhost>
2015-10-26 15:20:44 +0000

    Ignore

--- Papyrus.Desktop/Features/topics/TopicsGridVM.cs
+++ Papyrus.Desktop/Features/topics/TopicsGridVM.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
