e967d96f9f10761c0f04231c69e42767084688c7
Ronny Ancorini <devnull@localhost>
2015-10-27 11:15:50 +0000

    Refactor: remove duplications

--- Papyrus.Tests/Business/VersionRangeCollisionDetectorShould.cs
+++ Papyrus.Tests/Business/VersionRangeCollisionDetectorShould.cs
@@ -20,19 +20,27 @@ namespace Papyrus.Tests.Business
             new ProductVersion("version4", "4.0", DateTime.Today.AddDays(-4)),
             new ProductVersion("version5", "5.0", DateTime.Today.AddDays(-2)),
         };
+        private const string ProductId = "PapyrusId";
+
+        private VersionRangeCollisionDetector versionRangeCollisionDetector;
+        private ProductRepository productRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            productRepository = Substitute.For<ProductRepository>();
+            versionRangeCollisionDetector = new VersionRangeCollisionDetector(productRepository);
+            productRepository.GetAllVersionsFor(ProductId).Returns(Task.FromResult(productVersionsFrom1To5));
+        }
 
         [Test]
         public async Task detect_collision_when_toversion_of_a_version_range_is_equal_to_fromversion_in_any_other_range()
         {
-            var productRepository = Substitute.For<ProductRepository>();
-            var productId = "PapyrusId";
-            productRepository.GetAllVersionsFor(productId).Returns(Task.FromResult(productVersionsFrom1To5));
-            var topic = new Topic(productId);
+            var topic = new Topic(ProductId);
             topic.AddVersionRange(new VersionRange("version1", "version2"));
             topic.AddVersionRange(new VersionRange("version3", "version4"));
             topic.AddVersionRange(new VersionRange("version4", "version5"));
 
-            var versionRangeCollisionDetector = new VersionRangeCollisionDetector(productRepository);
             var isThereCollision = await versionRangeCollisionDetector.IsThereAnyCollisionFor(topic);
 
             isThereCollision.Should().BeTrue();
@@ -41,14 +49,10 @@ namespace Papyrus.Tests.Business
         [Test]
         public async Task detect_collision_when_fromversion_of_a_version_range_is_contained_by_other_range()
         {
-            var productRepository = Substitute.For<ProductRepository>();
-            const string productId = "PapyrusId";
-            productRepository.GetAllVersionsFor(productId).Returns(Task.FromResult(productVersionsFrom1To5));
-            var topic = new Topic(productId);
+            var topic = new Topic(ProductId);
             topic.AddVersionRange(new VersionRange("version1", "version3"));
             topic.AddVersionRange(new VersionRange("version2", "version4"));
 
-            var versionRangeCollisionDetector = new VersionRangeCollisionDetector(productRepository);
             var isThereCollision = await versionRangeCollisionDetector.IsThereAnyCollisionFor(topic);
 
             isThereCollision.Should().BeTrue();
