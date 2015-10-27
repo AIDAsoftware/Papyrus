95bb4690160408c7f56ca6b76af87f102c32dba6
Ronny Ancorini <devnull@localhost>
2015-10-27 11:08:20 +0000

    Refactor: extract field

--- Papyrus.Tests/Business/VersionRangeCollisionDetectorShould.cs
+++ Papyrus.Tests/Business/VersionRangeCollisionDetectorShould.cs
@@ -12,25 +12,21 @@ namespace Papyrus.Tests.Business
     [TestFixture]
     public class VersionRangeCollisionDetectorShould
     {
-        // TODO
-        //   - when from version of a version range is contained  by other range
-        // Examples:
-        //   (1-3), (2-4)
+        private List<ProductVersion> productVersionsFrom1To5 = new List<ProductVersion>
+        {
+            new ProductVersion("version1", "1.0", DateTime.Today.AddDays(-10)),
+            new ProductVersion("version2", "2.0", DateTime.Today.AddDays(-8)),
+            new ProductVersion("version3", "3.0", DateTime.Today.AddDays(-6)),
+            new ProductVersion("version4", "4.0", DateTime.Today.AddDays(-4)),
+            new ProductVersion("version5", "5.0", DateTime.Today.AddDays(-2)),
+        };
 
         [Test]
         public async Task detect_collision_when_toversion_of_a_version_range_is_equal_to_fromversion_in_any_other_range()
         {
-            var productVersions = new List<ProductVersion>
-            {
-                new ProductVersion("version1", "1.0", DateTime.Today.AddDays(-10)),
-                new ProductVersion("version2", "2.0", DateTime.Today.AddDays(-8)),
-                new ProductVersion("version3", "3.0", DateTime.Today.AddDays(-6)),
-                new ProductVersion("version4", "4.0", DateTime.Today.AddDays(-4)),
-                new ProductVersion("version5", "5.0", DateTime.Today.AddDays(-2)),
-            };
             var productRepository = Substitute.For<ProductRepository>();
             var productId = "PapyrusId";
-            productRepository.GetAllVersionsFor(productId).Returns(Task.FromResult(productVersions));
+            productRepository.GetAllVersionsFor(productId).Returns(Task.FromResult(productVersionsFrom1To5));
             var topic = new Topic(productId);
             topic.AddVersionRange(new VersionRange("version1", "version2"));
             topic.AddVersionRange(new VersionRange("version3", "version4"));
@@ -45,17 +41,9 @@ namespace Papyrus.Tests.Business
         [Test]
         public async Task detect_collision_when_fromversion_of_a_version_range_is_contained_by_other_range()
         {
-            var productVersions = new List<ProductVersion>
-            {
-                new ProductVersion("version1", "1.0", DateTime.Today.AddDays(-10)),
-                new ProductVersion("version2", "2.0", DateTime.Today.AddDays(-8)),
-                new ProductVersion("version3", "3.0", DateTime.Today.AddDays(-6)),
-                new ProductVersion("version4", "4.0", DateTime.Today.AddDays(-4)),
-                new ProductVersion("version5", "5.0", DateTime.Today.AddDays(-2)),
-            };
             var productRepository = Substitute.For<ProductRepository>();
             const string productId = "PapyrusId";
-            productRepository.GetAllVersionsFor(productId).Returns(Task.FromResult(productVersions));
+            productRepository.GetAllVersionsFor(productId).Returns(Task.FromResult(productVersionsFrom1To5));
             var topic = new Topic(productId);
             topic.AddVersionRange(new VersionRange("version1", "version3"));
             topic.AddVersionRange(new VersionRange("version2", "version4"));
