e63d4d4e2d797b29b9fb27fa051bcc29886625cc
Ronny Ancorini <devnull@localhost>
2015-10-28 22:34:27 +0000

    Refactor: move interfaces to its own file

---
+++ Papyrus.Business/Topics/TopicCommandRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Papyrus.Business.Topics
+{
+    public interface TopicCommandRepository
+    {
+        Task Save(Topic topic);
+        Task Update(Topic topic);
+        Task Delete(Topic topic);
+    }
+}
--- Papyrus.Business/Topics/TopicQueryRepository.cs
+++ Papyrus.Business/Topics/TopicQueryRepository.cs
@@ -3,13 +3,6 @@ using System.Threading.Tasks;
 
 namespace Papyrus.Business.Topics
 {
-    public interface TopicCommandRepository
-    {
-        Task Save(Topic topic);
-        Task Update(Topic topic);
-        Task Delete(Topic topic);
-    }
-
     public interface TopicQueryRepository
     {
         Task<List<TopicSummary>> GetAllTopicsSummaries();
