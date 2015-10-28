f027b79bb738013a2d35a9026ec9b7484c250015
Ronny Ancorini <devnull@localhost>
2015-10-28 11:55:12 +0000

    green: create_two_folders_for_spanish_and_english_documents_into_the_given _directory

--- Papyrus.Business/Exporters/MkDocsExporter.cs
+++ Papyrus.Business/Exporters/MkDocsExporter.cs
@@ -14,29 +14,34 @@ namespace Papyrus.Business.Exporters {
             this.repository = repository;
         }
 
-        public async Task ExportDocumentsToFolder(DirectoryInfo targetDirectory) {
-            if (!targetDirectory.Exists) targetDirectory.Create();
-            var topicSummaries = await repository.GetAllTopicsSummaries();
-            foreach (var topicSummary in topicSummaries) {
-                await ExportTopic(topicSummary, targetDirectory);
-            }
+        public async Task ExportDocumentsForProductToFolder(string papyrusid, DirectoryInfo targetDirectory) {
+            targetDirectory.CreateSubdirectory("Español");
+            targetDirectory.CreateSubdirectory("English");
         }
-
-        private async Task ExportTopic(TopicSummary topic, DirectoryInfo targetDirectory) {
-            //var fileName = topic.Title + ".md";
-            //var filePath = Path.Combine(targetDirectory.FullName, fileName);
-            //await WriteTextAsync(filePath, topic.Content);
-        }
-
-        private async Task WriteTextAsync(string filePath, string text) {
-            byte[] encodedText = Encoding.UTF8.GetBytes(text);
-
-            using (FileStream sourceStream = new FileStream(filePath,
-                FileMode.Append, FileAccess.Write, FileShare.None,
-                bufferSize: 4096, useAsync: true)) {
-                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
-            };
-        }
+        
+//        public async Task ExportDocumentsForProductToFolder(string papyrusid, DirectoryInfo targetDirectory) {
+//            if (!targetDirectory.Exists) targetDirectory.Create();
+//            var topicSummaries = await repository.GetAllTopicsSummaries();
+//            foreach (var topicSummary in topicSummaries) {
+//                await ExportTopic(topicSummary, targetDirectory);
+//            }
+//        }
+//
+//        private async Task ExportTopic(TopicSummary topic, DirectoryInfo targetDirectory) {
+//            //var fileName = topic.Title + ".md";
+//            //var filePath = Path.Combine(targetDirectory.FullName, fileName);
+//            //await WriteTextAsync(filePath, topic.Content);
+//        }
+//
+//        private async Task WriteTextAsync(string filePath, string text) {
+//            byte[] encodedText = Encoding.UTF8.GetBytes(text);
+//
+//            using (FileStream sourceStream = new FileStream(filePath,
+//                FileMode.Append, FileAccess.Write, FileShare.None,
+//                bufferSize: 4096, useAsync: true)) {
+//                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+//            };
+//        }
     
     }
 }
---
+++ Papyrus.Tests/Business/MkdocsExporterShould.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Papyrus.Business.Exporters;
+using Papyrus.Business.Topics;
+
+namespace Papyrus.Tests.Business
+{
+    [TestFixture]
+    public class MkdocsExporterShould
+    {
+        // TODO:
+        //   - create a folder for spanish and english documents
+        //   - insert in each of these folders topics its respective topics
+        [Test]
+        public async Task create_two_folders_for_spanish_and_english_documents_into_the_given_directory()
+        {
+            var testDirectory = Directory.CreateDirectory("test");
+            var topicRepository = Substitute.For<TopicRepository>();
+            var mkdocsExporter = new MkDocsExporter(topicRepository);
+
+            await mkdocsExporter.ExportDocumentsForProductToFolder("PapyrusId", testDirectory);
+
+            var languagesFolders = testDirectory.GetDirectories().Select(d => d.Name);
+            languagesFolders.Should().Contain("Español");
+            languagesFolders.Should().Contain("English");
+        }
+    }
+}
