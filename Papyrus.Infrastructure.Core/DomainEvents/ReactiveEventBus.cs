123f4b54c1e8a6c1368f581806943aaa96d2dc1a
Modesto San Juan <msanjuan@aidacanarias.com>
2015-11-09 23:21:01 +0000

    Added ReactiveEventBus to Papyrus.Infrastructure.Core

---
+++ Papyrus.Infrastructure.Core/DomainEvents/ReactiveEventBus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Papyrus.Infrastructure.Core.DomainEvents {
+    public static class ReactiveEventBus {
+        private static readonly Subject<object> messageSubject = new Subject<object>();
+
+        public static void Send<T>(T message) {
+            messageSubject.OnNext(message);
+        }
+
+        public static IObservable<T> AsObservable<T>() { return messageSubject.OfType<T>(); }
+    }
+}
