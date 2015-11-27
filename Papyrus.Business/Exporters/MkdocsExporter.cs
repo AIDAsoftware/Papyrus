9d2386a328cdded7cb5fe7d6e2d139be69783906
Ronny Ancorini <devnull@localhost>
2015-11-27 00:55:39 +0000

    Refactor: extract method

--- Papyrus.Business/Exporters/MkdocsExporter.cs
+++ Papyrus.Business/Exporters/MkdocsExporter.cs
@@ -5,17 +5,21 @@ using Papyrus.Business.Products;
 namespace Papyrus.Business.Exporters {
     public class MkdocsExporter {
         public async Task Export(WebSite webSite, string path) {
-            var directory = Directory.CreateDirectory(path);
+            var docsPath = Path.Combine(path, "docs");
+            var docsDirectory = Directory.CreateDirectory(docsPath);
             foreach (var document in webSite.documents) {
-                await ExportDocumentIn(document, directory);
+                await ExportDocumentIn(document, docsDirectory);
             }
+            await WriteYmlFileIn(path);
+        }
+
+        private static async Task WriteYmlFileIn(string path) {
             var ymlPath = Path.Combine(path, "mkdocs.yml");
             await FileWriter.WriteFileWithContent(ymlPath, "");
         }
 
         private static async Task ExportDocumentIn(ExportableDocument document, DirectoryInfo directory) {
-            var docsDirectory = directory.CreateSubdirectory("docs");
-            var documentDirectory = docsDirectory.CreateSubdirectory(document.Route);
+            var documentDirectory = directory.CreateSubdirectory(document.Route);
             var documentPath = Path.Combine(documentDirectory.FullName, document.Title + ".md");
             await FileWriter.WriteFileWithContent(documentPath, document.Content);
         }
