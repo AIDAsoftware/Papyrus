727bd702ffe4be4ce1aea33618c67217075ce8d7
Ronny Ancorini <devnull@localhost>
2015-12-01 09:41:50 +0000

    Refactor: clean tests

--- Papyrus.Tests/Business/WebConstructorShould.cs
+++ Papyrus.Tests/Business/WebConstructorShould.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NSubstitute;
@@ -12,10 +13,16 @@ using Papyrus.Business.Topics;
 namespace Papyrus.Tests.Business {
     [TestFixture]
     public class WebConstructorShould {
+        private const string EnglishContent = "Content";
+        private const string EnglishTitle = "Title";
+        private const string DocumentRoute = "DocumentRoute";
         private TopicQueryRepository topicRepo;
         private ProductRepository productRepo;
         private PathGenerator pathGenerator;
         private WebsiteConstructor websiteConstructor;
+        private string Spanish = "es-ES";
+        private string LastVersionName = "15.11.27";
+        private readonly ExportableDocument englishDocument = new ExportableDocument(EnglishTitle, EnglishContent, DocumentRoute);
         // TODO:
         //   - should construct a website with documents associated to given product, version and language
         //   - should get the route to construct the website in the website collection using the given strategy
@@ -30,52 +37,67 @@ namespace Papyrus.Tests.Business {
         
         [Test]
         public async Task construct_a_website_with_documents_associated_to_given_product_and_version() {
-            pathGenerator.GenerateMkdocsPath().Returns("Route/Route"); 
-            pathGenerator.GenerateDocumentRoute().Returns("DocumentRoute"); 
-            var versions = Versions("15.11.27");
-            var versionsNames = Names(versions);
-            var opportunity = new Product("OpportunityId", "Opportunity", versions);
-            productRepo.GetProductForVersions(opportunity.Id, versionsNames)
-                .Returns(Task.FromResult(opportunity));
-            var document = new ExportableDocument("Title", "Content", "DocumentRoute");
-            topicRepo.GetAllDocumentsFor(opportunity, "15.11.27", "es-ES").Returns(Task.FromResult(new List<ExportableDocument> { document }));
+            var versionsNames = VersionNames(LastVersionName);
+            var opportunity = new Product("OpportunityId", "Opportunity", VersionsFrom(versionsNames));
+            StubPathGeneratorToReturnAs(exportationPath: "Route/Route", documentRoute: DocumentRoute);
+            RepositoryReturnsProductWhenAskingForVersions(opportunity, versionsNames);
+            topicRepo.GetAllDocumentsFor(opportunity, LastVersionName, Spanish)
+                .Returns(AsyncDocumentsList(englishDocument));
 
-            var websites = await websiteConstructor.Construct(new List<string> { opportunity.Id }, versionsNames, Languages("es-ES"));
+            var websites = await websiteConstructor
+                .Construct(ProductsIdsList(opportunity), versionsNames, Languages(Spanish));
 
             ExportableDocument websiteDocument = websites["Route/Route"].Documents.First();
-            websiteDocument.Content.Should().Be("Content");
-            websiteDocument.Title.Should().Be("Title");
-            websiteDocument.Route.Should().Be("DocumentRoute");
-        }
-
-        private List<string> Languages(string language) {
-            return new List<string>{ language };
-        }
-
-        private static List<string> Names(IEnumerable<ProductVersion> versions) {
-            return versions.Select(x => x.VersionName).ToList();
-        }
-
-        private static List<ProductVersion> Versions(string versionName) {
-            return new List<ProductVersion>{ new ProductVersion("AnyId", versionName, DateTime.Today) };
+            websiteDocument.Content.Should().Be(EnglishContent);
+            websiteDocument.Title.Should().Be(EnglishTitle);
+            websiteDocument.Route.Should().Be(DocumentRoute);
         }
 
         [Test]
         public async Task construct_proper_keys_for_each_website() {
-            var versionsNames = new List<string>{ "15.11.27" };
-            var versions = new List<ProductVersion>{ new ProductVersion("AnyId", "15.11.27", DateTime.Today) };
-            var languages = new List<string>{ "es-ES" };
-            var opportunity = new Product("OpportunityId", "Opportunity", versions);
-            productRepo.GetProductForVersions("OpportunityId", versionsNames)
-                        .Returns(Task.FromResult(opportunity));
-            var document = new ExportableDocument("Title", "Content", "DocumentRoute");
-            topicRepo.GetAllDocumentsFor(opportunity, "15.11.27", "es-ES").Returns(Task.FromResult(new List<ExportableDocument>{ document }));
+            var versionsNames = VersionNames(LastVersionName);
+            var opportunity = new Product("OpportunityId", "Opportunity", VersionsFrom(versionsNames));
+            RepositoryReturnsProductWhenAskingForVersions(opportunity, versionsNames);
+            topicRepo.GetAllDocumentsFor(opportunity, LastVersionName, Spanish).Returns(AsyncDocumentsList(englishDocument));
 
-            await websiteConstructor.Construct(new List<string>{opportunity.Id}, versionsNames, languages);
+            await websiteConstructor.Construct(new List<string> { opportunity.Id }, versionsNames, Languages(Spanish));
 
             pathGenerator.Received().ForProduct(opportunity.Name);
-            pathGenerator.Received().ForVersion("15.11.27");
-            pathGenerator.Received().ForLanguage("es-ES");
+            pathGenerator.Received().ForVersion(LastVersionName);
+            pathGenerator.Received().ForLanguage(Spanish);
+        }
+
+        private List<ProductVersion> VersionsFrom(List<string> versionsNames) {
+            return versionsNames.Select(versionsName => 
+                    new ProductVersion(versionsName, versionsName, DateTime.Today))
+                .ToList();
+        }
+
+        private async static Task<List<ExportableDocument>> AsyncDocumentsList(ExportableDocument document) {
+            return new List<ExportableDocument>{ document };
+        }
+
+
+        private void RepositoryReturnsProductWhenAskingForVersions(Product opportunity, List<string> versionsNames) {
+            productRepo.GetProductForVersions(opportunity.Id, versionsNames)
+                .Returns(Task.FromResult(opportunity));
+        }
+
+        private List<string> Languages(string language) {
+            return new List<string> { language };
+        }
+
+        private static List<string> VersionNames(params string[] languages) {
+            return languages.ToList();
+        }
+
+        private static List<string> ProductsIdsList(Product opportunity) {
+            return new List<string> { opportunity.Id };
+        }
+
+        private void StubPathGeneratorToReturnAs(string exportationPath, string documentRoute) {
+            pathGenerator.GenerateMkdocsPath().Returns(exportationPath);
+            pathGenerator.GenerateDocumentRoute().Returns(documentRoute);
         }
     }
 }
