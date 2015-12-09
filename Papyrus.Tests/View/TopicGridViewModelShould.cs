bd3e9ae20d727e8816f634613f0910301bf59ecf
Ronny Ancorini <devnull@localhost>
2015-12-09 08:54:02 +0000

    To do list for Topic Grid VM

---
+++ Papyrus.Tests/View/TopicGridViewModelShould.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Papyrus.Tests.View {
+    
+    //TODO:
+    // - export all documentation for all products
+    // - export only last version for selected product
+    // - export all documentation for selected product
+    
+    [TestFixture]
+    public class TopicGridViewModelShould {
+        
+    }
+}
