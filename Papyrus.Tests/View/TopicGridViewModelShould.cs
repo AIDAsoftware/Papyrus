920ffa4ee64332d5735bb39f88f6b29a3504376f
Ronny Ancorini <devnull@localhost>
2015-12-09 22:21:46 +0000

    Refactor: clean test

--- Papyrus.Tests/View/TopicGridViewModelShould.cs
+++ Papyrus.Tests/View/TopicGridViewModelShould.cs
@@ -18,31 +18,82 @@ namespace Papyrus.Tests.View {
     
     [TestFixture]
     public class TopicGridViewModelShould {
+        private readonly List<string> versions = new List<string> {
+            "15.11.27"
+        };
+        private readonly List<DisplayableProduct> allProducts = new List<DisplayableProduct> {
+            new DisplayableProduct {ProductId = OpportunityId, ProductName = "Any"}
+        };
+
+        private TopicQueryRepository topicRepo;
+        private ProductRepository productRepo;
+        private WebsiteConstructor websiteConstructor;
+        private MkdocsExporter exporter;
+        private readonly List<string> spanishAndEnlgishLanguages = new List<string>{"es-ES", "en-GB"};
+        private readonly WebSite WebsiteWithADocument = WebsiteWith(new ExportableDocument("Title", "content", ""));
+        private const string OpportunityId = "OpportunityId";
+
+        [SetUp]
+        public void SetUp() {
+            topicRepo = Substitute.For<TopicQueryRepository>();
+            productRepo = Substitute.For<ProductRepository>();
+            websiteConstructor = Substitute.For<WebsiteConstructor>(topicRepo, productRepo);
+            exporter = Substitute.For<MkdocsExporter>();
+        }
+
         [Test]
         public async Task export_All_documentation_for_all_products_in_spanish_and_english() {
-            var topicRepo = Substitute.For<TopicQueryRepository>();
-            var productRepo = Substitute.For<ProductRepository>();
-            var allVersions = new List<string> {
-                "15.11.27"
-            };
-            productRepo.GetAllVersionsNamesDistinctingByName().Returns(Task.FromResult(allVersions));
-            productRepo.GetAllDisplayableProducts().Returns(Task.FromResult(new List<DisplayableProduct> {
-                new DisplayableProduct {ProductId = "OpportunityId", ProductName = "Any"}
-            }));
-            var exporter = Substitute.For<MkdocsExporter>();
-            var websiteConstructor = Substitute.For<WebsiteConstructor>(topicRepo, productRepo);
-            var websiteCollection = new WebsiteCollection();
-            var webSite = new WebSite(new List<ExportableDocument> {
-                new ExportableDocument("Title", "content", "")
-            });
-            websiteCollection.Add("Any/Path", webSite);
-            websiteConstructor.Construct(Arg.Any<PathByVersionGenerator>(), Arg.Is<IEnumerable<Product>>(ps => ps.Any(p => p.Id == "OpportunityId")), allVersions, Arg.Is<List<string>>(ls => ls.Contains("es-ES") && ls.Contains("en-GB") && ls.Count == 2)).Returns(Task.FromResult(websiteCollection));
-            var viewModel = new TopicsGridVM(topicRepo, productRepo, exporter, websiteConstructor);
-            await viewModel.Initialize();
+            StubOutProductRepoToReturnAsAllVersions(versions);
+            StubOutProductRepoToReturnAsAllProducts(allProducts);
+            var websiteCollection = new WebsiteCollection {{"Any/Path", WebsiteWithADocument}};
+            WhenWebConstructorIsCalledWithVersionPathStrategyAnd(
+                allProducts, versions, spanishAndEnlgishLanguages
+            ).Returns(Task.FromResult(websiteCollection));
+            var viewModel = await InitializeTopicGridVMWith(topicRepo, productRepo, exporter, websiteConstructor);
 
+            await ExecuteExportAllProductsCommandFrom(viewModel);
+
+            exporter.Received().Export(WebsiteWithADocument, "Any/Path");
+        }
+
+        private static async Task ExecuteExportAllProductsCommandFrom(TopicsGridVM viewModel) {
             await viewModel.ExportAllProducts.ExecuteAsync(new object());
+        }
 
-            exporter.Received().Export(webSite, "Any/Path");
+        private async Task<TopicsGridVM> InitializeTopicGridVMWith(TopicQueryRepository topicRepo, ProductRepository productRepo, MkdocsExporter exporter, WebsiteConstructor constructor) {
+            var viewModel = new TopicsGridVM(topicRepo, productRepo, exporter, constructor);
+            await viewModel.Initialize();
+            return viewModel;
+        }
+
+        private async Task<WebsiteCollection> WhenWebConstructorIsCalledWithVersionPathStrategyAnd(List<DisplayableProduct> products, List<string> versions, List<string> languages) {
+            return await websiteConstructor.Construct(
+                Arg.Any<PathByVersionGenerator>(), 
+                Arg.Is<IEnumerable<Product>>(ps => AreEquivalent(ps, products)),
+                versions, 
+                Arg.Is<List<string>>(ls => ls.SequenceEqual(languages)));
+        }
+
+        private bool AreEquivalent(IEnumerable<Product> products, List<DisplayableProduct> displayableProducts) {
+            return products
+                .Select(x => x.Id)
+                .SequenceEqual(displayableProducts
+                                .Select(x => x.ProductId));
+        }
+
+        private static WebSite WebsiteWith(ExportableDocument document) {
+            var webSite = new WebSite(new List<ExportableDocument> {
+                document
+            });
+            return webSite;
+        }
+
+        private void StubOutProductRepoToReturnAsAllProducts(List<DisplayableProduct> products) {
+            productRepo.GetAllDisplayableProducts().Returns(Task.FromResult(products));
+        }
+
+        private void StubOutProductRepoToReturnAsAllVersions(List<string> versions) {
+            productRepo.GetAllVersionsNamesDistinctingByName().Returns(Task.FromResult(versions));
         }
     }
 }
