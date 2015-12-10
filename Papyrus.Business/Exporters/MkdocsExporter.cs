e474fa38c3f6a2cc16b76554aa10b25ba58d83ef
Ronny Ancorini <devnull@localhost>
2015-12-10 15:06:58 +0000

    Refactor: use System.Enviroment.NewLine instead of \n

--- Papyrus.Business/Exporters/MkdocsExporter.cs
+++ Papyrus.Business/Exporters/MkdocsExporter.cs
@@ -6,8 +6,8 @@ namespace Papyrus.Business.Exporters {
     public class MkdocsExporter {
         private const string YmlFileName = "mkdocs.yml";
         private const string MarkDownExtension = ".md";
-        private const string MkdocsTheme = "theme: readthedocs\n";
-        private const string SiteNAme = "site_name: SIMA Documentation\n";
+        private static readonly string MkdocsTheme = "theme: readthedocs" + System.Environment.NewLine;
+        private static readonly string SiteNAme = "site_name: SIMA Documentation" + System.Environment.NewLine;
 
         public virtual async Task Export(WebSite webSite, string path) {
             var docsPath = Path.Combine(path, "docs");
