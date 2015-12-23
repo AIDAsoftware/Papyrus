d7477397cea6b82e5493e0064d89a7290fff4004
Ronny Ancorini <devnull@localhost>
2015-12-23 09:44:34 +0000

    Refactor: extract fields

--- Papyrus.Business/Exporters/MkdocsFileNameConverter.cs
+++ Papyrus.Business/Exporters/MkdocsFileNameConverter.cs
@@ -2,18 +2,29 @@ using System.IO;
 
 namespace Papyrus.Business.Exporters {
     public class MkdocsFileNameConverter {
+        const string InvalidLetters = "áàäéèëíìïóòöúùuÁÀÄÉÈËÍÌÏÓÒÖÚÙÜçÇñ";
+        const string ValidLetters = "aaaeeeiiiooouuuAAAEEEIIIOOOUUUcCn";
+        private const string ValidChar = "-";
+
         public static string ConvertToValidFileName(string title) {
-            var invalidChars = Path.GetInvalidFileNameChars();
-            var exportableTitle = title;
-            foreach (var invalidChar in invalidChars) {
-                exportableTitle = exportableTitle.Replace(invalidChar.ToString(), "-");
-            }
-            var invalidLetters = "áàäéèëíìïóòöúùuÁÀÄÉÈËÍÌÏÓÒÖÚÙÜçÇñ";
-            var validLetters = "aaaeeeiiiooouuuAAAEEEIIIOOOUUUcCn";
-            for (int i = 0; i < invalidLetters.Length; i++) {
-                exportableTitle = exportableTitle.Replace(invalidLetters[i], validLetters[i]);
-            }
+            var exportableTitle = ReplaceInvalidCharactersForASystemFile(title);
+            exportableTitle = ReplaceInvalidCharactersForMkdocsFile(exportableTitle);
             return exportableTitle;
+        }
+
+        private static string ReplaceInvalidCharactersForMkdocsFile(string title) {
+            for (var i = 0; i < InvalidLetters.Length; i++) {
+                title = title.Replace(InvalidLetters[i], ValidLetters[i]);
+            }
+            return title;
+        }
+
+        private static string ReplaceInvalidCharactersForASystemFile(string title) {
+            var invalidChars = Path.GetInvalidFileNameChars();
+            foreach (var invalidChar in invalidChars) {
+                title = title.Replace(invalidChar.ToString(), ValidChar);
+            }
+            return title;
         }
     }
 }
