f399afe668a343dad9edc8e9298ae09e45c93653
Ronny Ancorini <devnull@localhost>
2016-01-26 08:48:29 +0000

    Code convention

--- Papyrus.Desktop/Features/topics/TopicsGrid.xaml.cs
+++ Papyrus.Desktop/Features/topics/TopicsGrid.xaml.cs
@@ -3,41 +3,33 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using Papyrus.Business.Documents;
 
-namespace Papyrus.Desktop.Features.Topics
-{
-    public partial class TopicsGrid : UserControl
-    {
-        public TopicsGridVM ViewModel
-        {
+namespace Papyrus.Desktop.Features.Topics {
+    public partial class TopicsGrid : UserControl {
+        public TopicsGridVM ViewModel {
             get { return (TopicsGridVM)DataContext; }
         }
 
         public DisplayableProduct SelectedProduct { get; set; }
 
-        public TopicsGrid()
-        {
+        public TopicsGrid() {
             InitializeComponent();
 
             DataContext = ViewModelsFactory.TopicsGrid();
             Loaded += TopicGrid_Loaded;
         }
 
-        private async void TopicGrid_Loaded(object sender, RoutedEventArgs e)
-        {
+        private async void TopicGrid_Loaded(object sender, RoutedEventArgs e) {
             await ViewModel.Initialize();
         }
 
-        private async void NewTopic_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (ViewModel.SelectedProduct != null)
-            {
+        private async void NewTopic_OnClick(object sender, RoutedEventArgs e) {
+            if (ViewModel.SelectedProduct != null) {
                 var topic = await ViewModel.PrepareNewDocument();
                 new TopicWindow(topic).Show();
             }
         }
 
-        private async void TopicRow_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
+        private async void TopicRow_OnMouseDoubleClick(object sender, MouseButtonEventArgs e) {
             var topicId = ViewModel.SelectedTopic.TopicId;
             var topic = await ViewModel.GetEditableTopicById(topicId);
             new TopicWindow(topic).Show();
