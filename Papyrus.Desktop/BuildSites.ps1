79b88515ff13df79254fe1d2bbbb10e905f0713f
rancorini <devnull@localhost>
2016-05-04 17:21:15 +0100

    Powershell file to build websites

---
+++ Papyrus.Desktop/BuildSites.ps1
@@ -0,0 +1,10 @@
+﻿$siteFiles = ls mkdocs.yml -recurse 
+$originalPath = pwd
+
+foreach($siteFile in $siteFiles) {
+    cd $siteFile.Directory;
+    Write-Host $siteFile.Directory;
+    Invoke-Expression "& mkdocs build"
+}
+
+cd $originalPath
