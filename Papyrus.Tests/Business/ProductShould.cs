adf2a9c3d6927bf87a159e96ace710e28f5f8387
Miguel Angel Viera <Miguel Angel Viera>
2016-07-17 22:07:50 +0100

    Refactor: clean test

--- Papyrus.Tests/Business/ProductShould.cs
+++ Papyrus.Tests/Business/ProductShould.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 using Papyrus.Business.Products;
@@ -9,30 +10,42 @@ namespace Papyrus.Tests.Business
     [TestFixture]
     public class ProductShould
     {
+        private readonly DateTime today = DateTime.Today;
+        private readonly DateTime yesterday = DateTime.Today.AddDays(-1);
+
         [Test]
         public void return_its_first_version()
         {
-            var firstVersion = new ProductVersion("Any", "Any", DateTime.Now.AddDays(-1));
-            var versions = new List<ProductVersion> {
-                firstVersion,
-                new ProductVersion("Any", "Any", DateTime.Now),
-            };
-            var product = new Product("AnyId", "AnyName", versions);
+            var product = ProductWith(
+                Versions(AVersionFrom(yesterday), AVersionFrom(today)));
 
-            product.FirstVersion().Should().Be(firstVersion);
+            product.FirstVersion().Should().Be(AVersionFrom(yesterday));
         }
 
         [Test]
         public void return_its_last_version()
         {
-            var lastVersion = new ProductVersion("Any", "Any", DateTime.Now);
-            var versions = new List<ProductVersion> {
-                new ProductVersion("Any", "Any", DateTime.Now.AddDays(-1)),
-                lastVersion,
-            };
-            var product = new Product("AnyId", "AnyName", versions);
+            var product = ProductWith(
+                Versions(AVersionFrom(yesterday), AVersionFrom(today)));
 
-            product.LastVersion().Should().Be(lastVersion);
+            product.LastVersion().Should().Be(AVersionFrom(today));
         }
+
+        private Product ProductWith(List<ProductVersion> versions)
+        {
+            return new Product("AnyId", "AnyName", versions);
+        }
+
+        private List<ProductVersion> Versions(params ProductVersion[] versions)
+        {
+            return versions.ToList();
+        }
+
+
+        private static ProductVersion AVersionFrom(DateTime dateTime)
+        {
+            return new ProductVersion("Any", "Any", dateTime);
+        }
+
     }
 }
