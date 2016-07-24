763f525265f1e90c6973b673cf7967ef7c2d4ff1
Miguel Angel Viera <mangelviera93@gmail.com>
2016-07-24 22:38:50 +0100

    Add todo

--- Papyrus.Business/DocumentDto.cs
+++ Papyrus.Business/DocumentDto.cs
@@ -1,4 +1,5 @@
 namespace Papyrus.Business {
+    // TODO: replace this class for Document (they are equal for now)
     public class DocumentDto {
         public string Title { get; set; }
         public string Description { get; set; }
