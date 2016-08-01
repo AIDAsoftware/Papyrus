126e844940fbbd2efd98a434ff7f3dad9f0e9260
Miguel Angel Viera <mangelviera93@gmail.com>
2016-08-01 21:36:49 +0100

    Clean unnecessary dependencies from Infrastructure Core

--- Papyrus.Infrastructure.Core/Database/DatabaseException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Papyrus.Infrastructure.Core.Database {
-    using System;
-
-    public class DatabaseException : Exception {
-        public DatabaseException(string message) : base(message) {}
-    }
-}
--- Papyrus.Infrastructure.Core/SharedDomain/Validation/ValidationError.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Papyrus.Infrastructure.Core.SharedDomain.Validation {
-    public class ValidationError {
-        public string PropertyName { get; set; }
-        public string MessageKey { get; set; }
-        public string DisplayMessage { get; set; }
-    }
-}
