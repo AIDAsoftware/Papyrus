8af21bf33355f368b6c8b9fd9e543cb6feafad91
Ronny Ancorini <Ronnygx@gmail.com>
2016-08-13 14:18:54 +0100

    Green: get_all_products

--- Papyrus.Tests/Business/PapyrusShould.cs
+++ Papyrus.Tests/Business/PapyrusShould.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using FluentAssertions;
 using NSubstitute;
+using NSubstitute.Core.Arguments;
 using NUnit.Framework;
 using Papyrus.Business.Actions;
 using Papyrus.Business.Domain.Documents;
@@ -13,13 +14,13 @@ using Given = Papyrus.Tests.Business.GivenFixture;
 namespace Papyrus.Tests.Business {
     [TestFixture]
     public class PapyrusShould {
-        private DocumentsRepository repository;
+        private DocumentsRepository documentsRepository;
         private Given given;
 
         [SetUp]
         public void SetUp() {
-            repository = Substitute.For<DocumentsRepository>();
-            given = new Given(repository);
+            documentsRepository = Substitute.For<DocumentsRepository>();
+            given = new Given(documentsRepository);
         }
 
         [Test]
@@ -42,17 +43,42 @@ namespace Papyrus.Tests.Business {
 
             ExecuteCreateDocument(documentDto);
 
-            repository.Received(1).CreateDocumentFor(documentDto.Equivalent());
+            documentsRepository.Received(1).CreateDocumentFor(documentDto.Equivalent());
+        }
+
+        [Test]
+        public void get_all_products() {
+            var version = new ProductVersion("any", "any");
+            var product = new Product("any", "any", new List<ProductVersion> {version});
+            var productsRepository = Substitute.For<ProductRepository>();
+            productsRepository.GetAllProducts().Returns(new List<Product> {product});
+
+            var getAllProducts = new GetProducts(productsRepository);
+            var products = getAllProducts.Execute();
+
+            products.Single().Should().Be(product);
         }
 
         private void ExecuteCreateDocument(DocumentDto documentDto) {
-            var createDocument = new CreateDocument(repository);
+            var createDocument = new CreateDocument(documentsRepository);
             createDocument.ExecuteFor(documentDto);
         }
 
         private List<Document> GetDocumentationFor(VersionIdentifier version) {
-            var getDocumentation = new GetDocumentation(repository);
+            var getDocumentation = new GetDocumentation(documentsRepository);
             return getDocumentation.ExecuteFor(version.ProductId, version.VersionId).ToList();
+        }
+    }
+
+    public class GetProducts {
+        private readonly ProductRepository productsRepository;
+
+        public GetProducts(ProductRepository productsRepository) {
+            this.productsRepository = productsRepository;
+        }
+
+        public IReadOnlyCollection<Product> Execute() {
+            return productsRepository.GetAllProducts();
         }
     }
 }
