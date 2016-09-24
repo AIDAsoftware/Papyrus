0ca95196776feddf0fbee2b714ea8f879b15bf20
Miguel Angel Viera <mangelviera93@gmail.com>
2016-09-24 20:18:45 +0100

    Add behaviour to create a document in the NewDocumentPage component

--- Papyrus.Web/Content/src/app/pages/NewDocumentPage.js
+++ Papyrus.Web/Content/src/app/pages/NewDocumentPage.js
@@ -2,51 +2,78 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const DocumentActions  = {
+    createDocument: (title, language, description, content) => {
+        console.log(title, language, description, content);
+    }
+};
 
 export default class NewDocumentPage extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            value: 1
+            title : '',
+            language: 'spanish',
+            description : '',
+            content : ''
         };
     }
 
-    style = {
-        container: {
-            textAlign: 'center'
-        }
+    languageChange = (event, index, language)  => {
+        this.setState({language});
     }
 
-    handleChange = (event, index, value)  => {
-        this.setState({value});
+    titleChange = (event, title) => {
+        this.setState({title});
+    }
+
+    descriptionChange = (event, description) => {
+        this.setState({description});
+    }
+
+    contentChange = (event, content) => {
+        this.setState({content});
+    }
+
+    createDocument = () => {
+        DocumentActions.createDocument(this.state.title, this.state.language, this.state.description, this.state.content);
     }
 
     render () {
         const languages = [
-            <MenuItem key={1} value={1} primaryText="Spanish" />,
-            <MenuItem key={2} value={2} primaryText="English" />
+            <MenuItem key={1} value={'spanish'} primaryText="Spanish" />,
+            <MenuItem key={2} value={'english'} primaryText="English" />
         ];
         return (
             <div >
                 <TextField
+                    onChange={this.titleChange}
+                    value={this.state.title}
                     hintText="Title"
                     fullWidth/>
                 <SelectField
-                    value={this.state.value}
-                    onChange={this.handleChange}
+                    value={this.state.language}
+                    onChange={this.languageChange}
                     fullWidth>
                     {languages}
                 </SelectField>
                 <TextField
+                    onChange={this.descriptionChange}
+                    value={this.state.description}
                     hintText="Description"
                     fullWidth/>
                 <TextField
+                    onChange={this.contentChange}
+                    value={this.state.content}
                     hintText="Content"
                     rows={4}
                     multiLine={true}
                     rowsMax={10}
                     fullWidth/>
+                <RaisedButton onClick={this.createDocument} label="Create"/>
             </div>
         );
 
