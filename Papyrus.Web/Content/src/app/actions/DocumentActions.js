d5373b36532fe6ebf34eadc8269c013c2cf2a80a
Miguel Angel Viera <mangelviera93@gmail.com>
2016-09-25 01:45:56 +0100

    createDocument now saves the using the documents WebAPI

--- Papyrus.Web/Content/src/app/actions/DocumentActions.js
+++ Papyrus.Web/Content/src/app/actions/DocumentActions.js
@@ -1,5 +1,18 @@
+import ajax from 'ajax';
+
 export default {
+
     createDocument: (title, language, description, content) => {
-        console.log(title, language, description, content);
+        ajax.post('http://localhost/Papyrus.Api/products/1/versions/1/documents',
+        {
+            title,
+            language,
+            description,
+            content
+        }, (response) => {
+            console.log(response);
+        }, (err) => {
+            console.log(err);
+        });
     }
 };
