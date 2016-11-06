affbc1c2775c127df9a2f36699e34e84dd8eefc1
Ronny Ancorini <ronnygx@gmail.com>
2016-11-06 20:37:25 +0000

    remove unnecessary files and folders

--- Papyrus.Web/Content/server.js
+++ Papyrus.Web/Content/server.js
@@ -1,30 +1,11 @@
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config');
-const internalIp = require('internal-ip');
 const express = require('express');
-const webpack = require('webpack');
 const path = require('path');
 
 const app = express();
-const compiler = webpack(config);
 
-const middleware = webpackDevMiddleware(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-  silent: false,
-  stats: { color: true }
-});
-
-app.use(middleware);
-app.use(webpackHotMiddleware(compiler));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './src/www/index.html'));
-});
+app.use('/', express.static('.'));
 
 const port = 8080;
-const ip = internalIp.v4();
 
 app.listen(port, (err) => {
   if (err) {
@@ -34,6 +15,5 @@ app.listen(port, (err) => {
 
   console.log(' --------------------------------------');
   console.log(`    Local: http://0.0.0.0:${port}`);
-  console.log(` External: http://${ip}:${port}`);
   console.log(' --------------------------------------');
 });
